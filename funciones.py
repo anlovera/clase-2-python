b0ceff9c266dc9ec3bd10c5776d93dc392582989
ANYUMAR <anlovera>
2024-09-07 09:50:59 -0400

    clase 2 python

---
+++ funciones.py
@@ -0,0 +1,30 @@
+#def NombreDeFuncion(Parametros):
+
+nombre="Anyumar"
+numero1 =2 
+numero2 =3
+
+def Saludo(): 
+    print("Hola mucho gusto" + nombre)
+    
+    Saludo ()
+    
+def Sumar(): 
+        num1=10
+        num2=20
+        #print (num1 + num2)
+        suma = num1 + num2
+        return Suma
+    
+    
+def Resta (numero1,numero2):
+    
+    resta = numero1 - numero2
+    return resta
+
+    
+        
+print (Resta(numero1,numero2)+10)        
+        
+#Sumar()     
+Saludo ()
---
+++ while.py
@@ -0,0 +1,8 @@
+#while (Condicion): 
+  #Lo que este dentro del while 
+  #se ejecuta hasta que la condicion sea falsa
+  
+edad = 10
+  
+while edad < 10:
+      print ("Hola")
